8307c7a45b14b406080b37c32cd553848e3c7b5a
Simon Gregory <simon@helvector.org>
2013-02-02 23:12:29 +0000

    Slimming the tests down to use one set of examples

--- roman-numerals/rb/spec/roman_numerals_spec.rb
+++ roman-numerals/rb/spec/roman_numerals_spec.rb
@@ -20,10 +20,60 @@ describe RomanNumerals do
                'VII'  => 7,
                'VIII' => 8,
                'IX'   => 9,
-               'X'    => 10
+               'X'    => 10,
+               'XIV'  => 14,
+               'XV'   => 15,
+               'XIX'  => 19,
+               'XXI' => 21,
+               'XXIV' => 24,
+               'XXVIII' => 28,
+               'XXXI' => 31,
+               'XXXVI' => 36,
+               'XXXIX' => 39,
+               'XL' => 40,
+               'XLII' => 42,
+               'XLV' => 45,
+               'XLVIII' => 48,
+               'XLIX' => 49,
+               'L' => 50,
+               'LI' => 51,
+               'LV' => 55,
+               'LIX' => 59,
+               'LX' => 60,
+               'LXI' => 61,
+               'LXV' => 65,
+               'LXIX' => 69,
+               'LXX' => 70,
+               'LXXI' => 71,
+               'LXXV' => 75,
+               'LXXIX' => 79,
+               'LXXX' => 80,
+               'LXXXI' => 81,
+               'LXXXV' => 85,
+               'LXXXIX' => 89,
+               'XC' => 90,
+               'XCI' => 91,
+               'XCV' => 95,
+               'XCIX' => 99,
+               'C' => 100,
+               'CI' => 101,
+               'CIX' => 109,
+               'CXI' => 111,
+               'CXIX' => 119,
+               'CXXXVIII' => 138,
+               'CXLIX' => 149,
+               'CL' => 150,
+               'CLXIII' => 163,
+               'CC' => 200,
+               'CD' => 400,
+               'D' => 500,
+               'CMXCVIII' => 998,
+               'M' => 1000,
+               'MMX' => 2010,
+               'MMXIII' => 2013
              }
 
-  context "up to ten" do
+  context "up to twenty" do
 
     examples.each do |numeral, integer|
 
@@ -33,88 +83,5 @@ describe RomanNumerals do
 
     end
 
-  end
-
-  context "between ten and thirty nine" do
-
-    it "converts numbers in the tens" do
-      @numerals.convert(14).should == 'XIV'
-      @numerals.convert(15).should == 'XV'
-      @numerals.convert(19).should == 'XIX'
-    end
-
-    it "converts numbers in the twentys" do
-      @numerals.convert(21).should == 'XXI'
-      @numerals.convert(24).should == 'XXIV'
-      @numerals.convert(28).should == 'XXVIII'
-    end
-
-    it "converts numbers in the thirtys" do
-      @numerals.convert(31).should == 'XXXI'
-      @numerals.convert(36).should == 'XXXVI'
-      @numerals.convert(39).should == 'XXXIX'
-    end
-  end
-
-  context "between forty and fifty" do
-
-    it "converts numbers in the fortys" do
-      @numerals.convert(40).should == 'XL'
-      @numerals.convert(42).should == 'XLII'
-      @numerals.convert(45).should == 'XLV'
-      @numerals.convert(48).should == 'XLVIII'
-      @numerals.convert(49).should == 'XLIX'
-    end
-  end
-
-  context "between fifty and ninety" do
-
-    it "converts numbers in the fiftys" do
-      @numerals.convert(50).should == 'L'
-      @numerals.convert(51).should == 'LI'
-      @numerals.convert(55).should == 'LV'
-      @numerals.convert(59).should == 'LIX'
-
-      @numerals.convert(60).should == 'LX'
-      @numerals.convert(61).should == 'LXI'
-      @numerals.convert(65).should == 'LXV'
-      @numerals.convert(69).should == 'LXIX'
-
-      @numerals.convert(70).should == 'LXX'
-      @numerals.convert(71).should == 'LXXI'
-      @numerals.convert(75).should == 'LXXV'
-      @numerals.convert(79).should == 'LXXIX'
-
-      @numerals.convert(80).should == 'LXXX'
-      @numerals.convert(81).should == 'LXXXI'
-      @numerals.convert(85).should == 'LXXXV'
-      @numerals.convert(89).should == 'LXXXIX'
-    end
-  end
-
-  context "between ninety and one hundred" do
-    it "converts numbers in the nineties" do
-      @numerals.convert(90).should == 'XC'
-      @numerals.convert(91).should == 'XCI'
-      @numerals.convert(95).should == 'XCV'
-      @numerals.convert(99).should == 'XCIX'
-    end
-  end
-
-  context "between one hundred and four hundred" do
-    it "converts numbers" do
-      @numerals.convert(100).should == 'C'
-      @numerals.convert(101).should == 'CI'
-      @numerals.convert(109).should == 'CIX'
-      @numerals.convert(111).should == 'CXI'
-      @numerals.convert(119).should == 'CXIX'
-      @numerals.convert(138).should == 'CXXXVIII'
-      @numerals.convert(149).should == 'CXLIX'
-      @numerals.convert(150).should == 'CL'
-      @numerals.convert(163).should == 'CLXIII'
-      @numerals.convert(200).should == 'CC'
-      @numerals.convert(400).should == 'CD'
-      @numerals.convert(500).should == 'D'
-    end
   end
 end
