3030efd921cb4083bc1d7e71928b4e3d68bb5154
Simon Gregory <simon@helvector.org>
2013-02-02 17:21:05 +0000

    Passing from I to XX

--- roman-numerals/rb/lib/roman_numerals.rb
+++ roman-numerals/rb/lib/roman_numerals.rb
@@ -1,6 +1,6 @@
 class RomanNumerals
 
-  NUMERALS = {
+  ONE_TO_TEN = {
     :I    => 1,
     :II   => 2,
     :III  => 3,
@@ -10,13 +10,28 @@ class RomanNumerals
     :VII  => 7,
     :VIII => 8,
     :IX   => 9,
-    :X    => 10,
+    :X    => 10 }
+
+    OVER_TEN = {
     :L    => 50,
     :C    => 100,
     :D    => 500,
     :M    => 1000 }
 
   def convert(num)
-    NUMERALS.each { |k,v| return k.to_s if v == num }
+    ten_count = num/10
+    remainder = num%10
+
+    tens = 'X'*ten_count
+    ones = find(remainder)
+
+    tens + ones
+  end
+
+  private
+
+  def find(num)
+    ONE_TO_TEN.each { |k,v| return k.to_s if v == num }
+    ''
   end
 end
