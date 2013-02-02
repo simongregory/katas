eaf5bb758e5eaa1de2232b2eab479d122f75e811
Simon Gregory <simon@helvector.org>
2013-02-02 14:44:27 +0000

    Passing spare test

--- bowling/rb/lib/bowling_game.rb
+++ bowling/rb/lib/bowling_game.rb
@@ -1,11 +1,27 @@
-class BowlingGame
-  attr_reader :score
+require "enumerator"
 
+class BowlingGame
   def initialize
-    @score = 0
+    @scores = []
   end
 
   def roll(pins)
-    @score += pins
+    @scores << pins
+  end
+
+  def score
+    total = 0
+    spare = false
+
+    @scores.each_slice(2) do |frame|
+      frame_total = frame.inject(0, :+)
+
+      total += frame_total
+      total += frame[0] if spare
+
+      spare = (frame_total == 10)
+    end
+
+    total
   end
 end
