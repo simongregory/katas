1eaf3105a20da1ba0f3fb573010564a41e49a48f
Simon Gregory <simon@helvector.org>
2013-02-02 17:19:39 +0000

    Extending the numerals test up to XX

--- roman-numerals/rb/spec/roman_numerals_spec.rb
+++ roman-numerals/rb/spec/roman_numerals_spec.rb
@@ -11,19 +11,38 @@ describe RomanNumerals do
     @numerals = nil
   end
 
-  examples = { 'I'   => 1,
-               'II'  => 2,
-               'III' => 3,
-               'IV'  => 4,
-               'V'   => 5 }
+  examples = { 'I'    => 1,
+               'II'   => 2,
+               'III'  => 3,
+               'IV'   => 4,
+               'V'    => 5,
+               'VI'   => 6,
+               'VII'  => 7,
+               'VIII' => 8,
+               'IX'   => 9,
+               'X'    => 10
+             }
 
-  describe "turning integers into roman numerals" do
+  context "up to ten" do
 
     examples.each do |numeral, integer|
+
       it "converts #{integer} into #{numeral}" do
         @numerals.convert(integer).should == numeral
       end
+
     end
 
+  end
+
+  context "between ten and twenty" do
+
+    it "converts 14 to XIV" do
+      @numerals.convert(14).should == 'XIV'
+    end
+
+    it "converts 19 to XIX" do
+      @numerals.convert(19).should == 'XIX'
+    end
   end
 end
