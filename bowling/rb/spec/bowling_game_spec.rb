144d54ca33f54b8abd85744ffbeb985901447b35
Simon Gregory <simon@helvector.org>
2013-02-02 12:03:04 +0000

    Failing test for all bowls hitting the gutter

--- bowling/rb/spec/bowling_game_spec.rb
+++ bowling/rb/spec/bowling_game_spec.rb
@@ -2,4 +2,13 @@ require 'rspec'
 require 'bowling_game'
 
 describe BowlingGame do
+
+  describe "score" do
+
+    it "scores 0 when all bowls hit the gutter" do
+      bowling_game = BowlingGame.new
+      bowling_game.score.should == 0
+    end
+
+  end
 end
