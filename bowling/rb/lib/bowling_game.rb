7558b6e910fa9a51954691919bac89b767580a8f
Simon Gregory <simon@helvector.org>
2013-02-02 15:04:50 +0000

    Passing strikes

--- bowling/rb/lib/bowling_game.rb
+++ bowling/rb/lib/bowling_game.rb
@@ -7,19 +7,23 @@ class BowlingGame
 
   def roll(pins)
     @scores << pins
+    @scores << 0 if pins == 10
   end
 
   def score
     total = 0
     spare = false
+    strike = false
 
     @scores.each_slice(2) do |frame|
       frame_total = frame.inject(0, :+)
 
       total += frame_total
       total += frame[0] if spare
+      total += frame[1] if strike
 
       spare = (frame_total == 10)
+      strike = (frame[0] == 10)
     end
 
     total
