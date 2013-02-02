25da24aee5c98fc08663b2065f39a9a5ed6b2c4b
Simon Gregory <simon@helvector.org>
2013-02-02 16:38:32 +0000

    I to V passing

--- roman-numerals/rb/lib/roman_numerals.rb
+++ roman-numerals/rb/lib/roman_numerals.rb
@@ -1,23 +1,22 @@
 class RomanNumerals
 
   NUMERALS = {
-    'I' => 1,
-    'V' => 5,
-    'X' => 10,
-    'L' => 50,
-    'C' => 100,
-    'D' => 500,
-    'M' => 1000 }
+    :I    => 1,
+    :II   => 2,
+    :III  => 3,
+    :IV   => 4,
+    :V    => 5,
+    :VI   => 6,
+    :VII  => 7,
+    :VIII => 8,
+    :IX   => 9,
+    :X    => 10,
+    :L    => 50,
+    :C    => 100,
+    :D    => 500,
+    :M    => 1000 }
 
   def convert(num)
-    numeral = ''
-
-    if num == 4
-      numeral = 'IV'
-    else
-      num.times { numeral << 'I'}
-    end
-
-    numeral
+    NUMERALS.each { |k,v| return k.to_s if v == num }
   end
 end
