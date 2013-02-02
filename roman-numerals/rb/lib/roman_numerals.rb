bf59f8b70c080c2d26dc931c3c849299cad18968
Simon Gregory <simon@helvector.org>
2013-02-02 16:16:41 +0000

    Passing on IV

--- roman-numerals/rb/lib/roman_numerals.rb
+++ roman-numerals/rb/lib/roman_numerals.rb
@@ -1,7 +1,22 @@
 class RomanNumerals
+
+  NUMERALS = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000 }
+
   def convert(num)
     numeral = ''
-    num.times { numeral << 'I'}
+
+    if num == 4
+      numeral = 'IV'
+    else
+      num.times { numeral << 'I'}
+    end
 
     numeral
   end
