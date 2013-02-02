88b0add3bc86eac0444cd979ce82133d033dc048
Simon Gregory <simon@helvector.org>
2013-02-02 12:10:49 +0000

    Failing test for all bowls taking 1 pin down

--- bowling/rb/spec/bowling_game_spec.rb
+++ bowling/rb/spec/bowling_game_spec.rb
@@ -3,11 +3,23 @@ require 'bowling_game'
 
 describe BowlingGame do
 
+  before do
+    @bowling_game = BowlingGame.new
+  end
+
+  after do
+    @bowling_game = nil
+  end
+
   describe "score" do
 
     it "scores 0 when all bowls hit the gutter" do
-      bowling_game = BowlingGame.new
-      bowling_game.score.should == 0
+      @bowling_game.score.should == 0
+    end
+
+    it "scores 20 when all bowls take a pin down" do
+      20.times { @bowling_game.roll(1) }
+      @bowling_game.score.should == 20
     end
 
   end
