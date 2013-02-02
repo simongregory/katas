054c6087e0437c4b2d9c6fe1b9cc4f58c1d33eb9
Simon Gregory <simon@helvector.org>
2013-02-02 23:12:08 +0000

    Refactoring the conversion method to use the simple established rule

--- roman-numerals/rb/lib/roman_numerals.rb
+++ roman-numerals/rb/lib/roman_numerals.rb
@@ -1,55 +1,20 @@
 class RomanNumerals
 
-  ONE_TO_TEN = {
-    :I    => 1,
-    :II   => 2,
-    :III  => 3,
-    :IV   => 4,
-    :V    => 5,
-    :VI   => 6,
-    :VII  => 7,
-    :VIII => 8,
-    :IX   => 9,
-    :X    => 10 }
-
-  #L = 50, C = 100, D = 500, M = 1000
+  #I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+  NUMBERS  = [1000, 900, 500, 400,  100,  90,   50,   40,  10,    9,  5,    4,    1]
+  NUMERALS = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
   def convert(num)
     numeral = ''
 
-    if num > 99
-      numeral << 'C'
-      num -= 100
-    end
+    NUMBERS.each_index { |i|
+      boundary = NUMBERS[i]
+      while num >= boundary
+        numeral << NUMERALS[i]
+        num -= boundary
+      end
+    }
 
-    if num > 89
-      numeral << 'XC'
-      num -= 90
-    end
-
-    if num > 49
-      numeral << 'L'
-      num -= 50
-    end
-
-    if num > 39
-      numeral << 'XL'
-      num -= 40
-    end
-
-    while num > 9
-      numeral << 'X'
-      num -= 10
-    end
-
-    numeral << find(num)
     numeral
-  end
-
-  private
-
-  def find(num)
-    ONE_TO_TEN.each { |k,v| return k.to_s if v == num }
-    ''
   end
 end
