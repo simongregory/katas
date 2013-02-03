47070576e9300edf6f5fcd28bf5b84d0bfc4946d
Simon Gregory <simon@helvector.org>
2013-02-03 20:13:47 +0000

    Use zip to nip through the arrays and change a few names for clarity

--- roman-numerals/rb/lib/roman_numerals.rb
+++ roman-numerals/rb/lib/roman_numerals.rb
@@ -1,36 +1,37 @@
 class RomanNumerals
 
-  UNITS    = [1000, 900, 500, 400,  100,  90,   50,   40,  10,    9,  5,    4,    1]
+  ARABIC   = [1000, 900, 500, 400,  100,  90,   50,   40,  10,    9,  5,    4,    1]
   NUMERALS = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
-  def convert(num)
+  def convert(number)
     numeral = ''
 
-    UNITS.each_index { |i|
-      boundary = UNITS[i]
-      while num >= boundary
-        numeral << NUMERALS[i]
-        num -= boundary
+    ARABIC.zip(NUMERALS).each do |arabic, symbol|
+      while number >= arabic
+        numeral << symbol
+        number -= arabic
       end
-    }
+    end
 
     numeral
   end
 
   def revert(numeral)
-    total = 0
+    number = 0
+    rgx  = /(#{NUMERALS.join('|')})/
 
-    list = numeral.split(/(#{NUMERALS.join('|')})/)
-    list.each { |e| total += find_value(e) }
+    numeral.split(rgx).each do |symbol|
+      number += find_arabic_for(symbol)
+    end
 
-    total
+    number
   end
 
   private
 
-  def find_value(n)
+  def find_arabic_for(numeral)
     NUMERALS.each_index { |i|
-      return UNITS[i] if NUMERALS[i] == n
+      return ARABIC[i] if NUMERALS[i] == numeral
     }
     0
   end
